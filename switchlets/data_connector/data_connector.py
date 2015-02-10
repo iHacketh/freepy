6d04480c2116f0480141a68716e971bffb58d170
codish <codish@codish.(none)>
2015-02-10 14:15:07 +0530

    Data connector changes: 1. Moving out the switchlet 2. Using a more general design    for querying models. Previous    version had elements of the    incoming call logic in it

--- switchlets/data_connector/data_connector.py
+++ switchlets/data_connector/data_connector.py
@@ -25,30 +25,51 @@ from data_set import app_data, ivr_info, number_mappings
 
 import logging
 
-class DataStoreContext(object):
-  def __init__(self, caller_ref, context):
-    self.__caller_ref__ = caller_ref
-    self.__context__ = context
+class QueryContext(object):
+  def __init__(self, **kwargs):
+    self.__logger__ = logging.getLogger('call_handlers.data_connector.QueryContext')
+    try:
+      self.__caller_ref__ = kwargs['caller_ref']
+      self.__model__ = kwargs['model']
+      self.__key__ = kwargs['key']
+      self.__failure_destination_state__ = kwargs['failure_destination_state']
+      self.__success_destination_state__ = kwargs['success_destination_state']
+    except KeyError, e:
+      self.__logger__.error('Query Context initialized without mandatory parameter %s' % e)
+      raise e
 
   def get_caller_ref(self):
     return self.__caller_ref__
 
-  def get_context(self):
-    return self.__context__
+  def get_model(self):
+    return self.__model__
 
-class CallExecution(object):
-  def __init__(self, task_list):
-    self.__task_list__ = task_list
+  def get_key(self):
+    return self.__key__
 
-class QueryError(object):
-  pass
+  def get_failure_destination_state(self):
+      return self.__failure_destination_state__
 
-class QuerySuccess(object):
-  def __init__(self, query_result):
+  def get_success_destination_state(self):
+      return self.__success_destination_state__
+
+class QueryResult(object):
+  def __init__(self, query_result, message, destination_state):
     self.__query_result__ = query_result
+    self.__message__ = message
+    self.__destination_state__ = destination_state
 
   def get_query_result(self):
     return self.__query_result__
+
+  def get_message(self):
+    return self.__message__
+
+  def get_destination_state(self):
+    return self.__destination_state__
+
+class QueryDone(object):
+  pass
 
 class DataConnector(FiniteStateMachine, Switchlet):
   initial_state = 'not ready'
@@ -61,7 +82,7 @@ class DataConnector(FiniteStateMachine, Switchlet):
   
   def __init__(self, *args, **kwargs):
     super(DataConnector, self).__init__(*args, **kwargs)
-    self.__logger__ = logging.getLogger('call_handlers.data_connector')
+    self.__logger__ = logging.getLogger('call_handlers.data_connector.DataConnector')
     self.connect_to_data_store()
     
 
@@ -79,22 +100,32 @@ class DataConnector(FiniteStateMachine, Switchlet):
     self.transition(to = 'connected')
 
   @Action(state = 'querying')
-  def query_data_store(self, message):
+  def run_query(self, message):
     caller_ref = message.get_caller_ref()
-    context = message.get_context()
+    model = message.get_model()
+    key = message.get_key()
     try:
-      to_number = context.get('to')
-      app = self.__number_mappings__.get(to_number)
-      tasks = self.__app_data__.get(app)
+      # This may look really hacky. But it makes much more sense to design code this
+      # way if you want to query data from a real datastore in the future as opposed to 
+      # how it is being done now from a python variable
+      query = model + '.get("' + key + '")\n'
+      result = eval(query)
+      result_message = 'Query Successful for key %s in model %s' % (key, model)
+      destination_state = message.get_success_destination_state()
     except KeyError:
-      self.__logger__.error('App improperly configured for to number %s' % to_number)
-      query_error = QueryError()
-      caller_ref.tell({'content': query_error})
-      return
-    query_success = QuerySuccess(tasks)
-    caller_ref.tell({ 'content': query_success})
+      result_message = 'Query failed. No key %s in model %s' % (key, model)
+      result = None
+      destination_state = message.get_failure_destination_state()
+      self.__logger__.error(message)
+
+    query_result = QueryResult(result, result_message, destination_state)
+    caller_ref.tell({ 'content': query_result})
+    query_done = QueryDone()
+    self.actor_ref.tell({ 'content': query_done})
 
   def on_receive(self, message):
     message = message.get('content')
-    if isinstance(message, DataStoreContext):
+    if isinstance(message, QueryContext):
       self.transition(to = 'querying', event = message)
+    if isinstance(message, QueryContext):
+      self.transition(to = 'connected')
